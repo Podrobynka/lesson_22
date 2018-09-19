42870410aae79146d44d3b73efe273ace938231d
Podrobynka <yfgurda@gmail.com>
2018-09-20 01:05:31 +0300

    ...

--- app.rb
+++ app.rb
@@ -1,27 +1,9 @@
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/reloader'
-
-# configure do
-#   enable :sessions
-# end
-
-# helpers do
-#   def username
-#     session[:identity] ? session[:identity] : 'Hello stranger'
-#   end
-# end
-
-# before '/secure/*' do
-#   unless session[:identity]
-#     session[:previous_url] = request.path
-#     @error = 'Sorry, you need to be logged in to visit ' + request.path
-#     halt erb(:login_form)
-#   end
-# end
+# require 'sinatra/reloader'
 
 get '/' do
-  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+  erb 'Can you handle a?'
 end
 
 get '/about' do
@@ -32,25 +14,27 @@ get '/visit' do
   erb :visit
 end
 
+post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+
+  @title = 'Thank you!'
+  @message = "Dear #{@username}, we'll waiting for you at #{@datetime}."
+  f = File.open('public/users.txt', 'a')
+  f.write "\nUser: #{@username}. Phone: #{@phone}. Date and time: #{@datetime}"
+  f.close
+end
+
 get '/contacts' do
   erb :contacts
 end
 
-# get '/login/form' do
-#   erb :login_form
-# end
+post '/contacts' do
+  @email = params[:email]
+  @text = params['text']
 
-# post '/login/attempt' do
-#   session[:identity] = params['username']
-#   where_user_came_from = session[:previous_url] || '/'
-#   redirect to where_user_came_from
-# end
-
-# get '/logout' do
-#   session.delete(:identity)
-#   erb "<div class='alert alert-message'>Logged out</div>"
-# end
-
-# get '/secure/place' do
-#   erb 'This is a secret place that only <%=session[:identity]%> has access to!'
-# end
+  f = File.open('public/contacts.txt', 'a')
+  f.write "\n#{@email} - #{@text}"
+  f.close
+end
